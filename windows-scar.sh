8076106a238a185c3184b35b14004975ec0d3f6e
davidRichard23 <d2391@me.com>
2020-08-20 20:04:13 -0700

    .

---
+++ windows-scar.sh
@@ -0,0 +1,13 @@
+curl https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe
+ls
+python-3.8.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+# pip3 install wheel
+# pip3 install pyinstaller
+
+# git clone https://github.com/grycap/scar.git
+# cd scar
+# pip3 install -r requirements.txt
+
+# $HOME/.local/bin/pyinstaller --noconfirm --onefile scar/scarcli.py
+# chmod +x dist/scarcli
