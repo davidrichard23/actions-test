80289d007c6e139f835c1e0bfdea45a757144597
davidRichard23 <d2391@me.com>
2020-08-20 19:23:23 -0700

    .

---
+++ linux-scar.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install python3.8
+
+pip3 install pyinstaller
+
+git clone https://github.com/grycap/scar.git
+cd scar
+pip3 install -r requirements.txt
+
+pyinstaller --noconfirm --onefile scar/scarcli.py
+chmod +x dist/scarcli
