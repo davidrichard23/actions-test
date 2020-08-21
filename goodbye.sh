9e84609d2c007698b23fdc877297e9053ee3a254
davidRichard23 <d2391@me.com>
2020-08-20 18:34:05 -0700

    .

--- goodbye.sh
+++ goodbye.sh
@@ -6,9 +6,5 @@ git clone https://github.com/grycap/scar.git
 cd scar
 pip3 install -r requirements.txt
 
-ls
-
 pyinstaller --onefile scar/scarcli.py
-
-echo "dist dir"
-ls dist
+chmod +x dist/scarcli
