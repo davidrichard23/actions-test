4922aaf81ebcc4d914bbffeb005ecb8d7eb7317a
davidRichard23 <d2391@me.com>
2020-08-21 16:33:46 -0700

    update

--- windows-scar.ps1
+++ windows-scar.ps1
@@ -1,8 +1,8 @@
-echo "start"
+Write-Host "start"
 Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe -OutFile python-3.8.5-amd64.exe
-echo "downloaded"
-python-3.8.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-echo "installed"
+Write-Host "downloaded"
+./python-3.8.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+Write-Host "installed"
 
 # pip3 install wheel
 # pip3 install pyinstaller
