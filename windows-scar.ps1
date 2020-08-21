393fa5d9ec1024648635a9e050bf1c8922d3d157
davidRichard23 <d2391@me.com>
2020-08-21 16:46:19 -0700

    wip

--- windows-scar.ps1
+++ windows-scar.ps1
@@ -1,15 +1,13 @@
 Write-Host "start"
 Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.8.5/python-3.8.5-amd64.exe -OutFile python-3.8.5-amd64.exe
-Write-Host "downloaded"
 ./python-3.8.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-Write-Host "installed"
 
 # pip3 install wheel
-# pip3 install pyinstaller
+pip3 install pyinstaller
 
-# git clone https://github.com/grycap/scar.git
-# cd scar
-# pip3 install -r requirements.txt
+git clone https://github.com/grycap/scar.git
+cd scar
+pip3 install -r requirements.txt
 
-# $HOME/.local/bin/pyinstaller --noconfirm --onefile scar/scarcli.py
+$HOME/.local/bin/pyinstaller --noconfirm --onefile scar/scarcli.py
 # chmod +x dist/scarcli
