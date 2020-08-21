cb27797ecc02ce9fd6dd69c2ea6317ee4af226cc
davidRichard23 <d2391@me.com>
2020-08-20 18:26:29 -0700

    .

--- goodbye.sh
+++ goodbye.sh
@@ -1,25 +1,14 @@
 brew install python@3.8
 
 pip3 install pyinstaller
-pip3 install -U setuptools
-pip3 install scar
+
+git clone https://github.com/grycap/scar.git
+cd scar
+pip3 install -r requirements.txt
 
 ls
 
-pyinstaller --onefile $HOME/Library/Python/3.8/site-packages/scar/scarcli.py
-
-echo "1"
-ls $HOME/Library
-
-echo "2"
-ls $HOME/Library/Python
-
-echo "3"
-ls $HOME/Library/3.8
-
-echo "4"
-ls $HOME/Library/3.8/site-packages
-
+pyinstaller --onefile scar/scarcli.py
 
 echo "dist dir"
 ls dist
